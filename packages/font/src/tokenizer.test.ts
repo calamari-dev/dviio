202b79bf6ed2976eaac61f12dbad6bf919dd5e92
calamari-dev <pacific.flying.squid+calamari@gmail.com>
2022-04-17 01:13:18 +0900

    fix

--- packages/font/src/tokenizer.test.ts
+++ packages/font/src/tokenizer.test.ts
@@ -1,7 +1,15 @@
 import type { Token } from "./types";
 import { tokenizer } from "./tokenizer";
 
-const code = `
+const factrion = `
+/fact { %def
+  dup 1 gt { %if
+    dup 1 sub fact mul
+  } if
+} def
+`;
+
+const helloWorld = `
 /font /Courier findfont 24 scalefont def
 font setfont
 100 100 moveto
@@ -12,7 +20,33 @@ showpage
 it("tokenizer", async () => {
   const list: Token[] = [];
 
-  for await (const token of tokenizer(code)) {
+  for await (const token of tokenizer(factrion)) {
+    list.push(token);
+  }
+
+  expect(list).toEqual([
+    { type: "LITERAL_NAME", value: "fact" },
+    { type: "OPEN_CURLY_BRACKET" },
+    { type: "COMMENT", value: "def" },
+    { type: "EXECUTABLE_NAME", value: "dup" },
+    { type: "NUMBER", value: 1 },
+    { type: "EXECUTABLE_NAME", value: "gt" },
+    { type: "OPEN_CURLY_BRACKET" },
+    { type: "COMMENT", value: "if" },
+    { type: "EXECUTABLE_NAME", value: "dup" },
+    { type: "NUMBER", value: 1 },
+    { type: "EXECUTABLE_NAME", value: "sub" },
+    { type: "EXECUTABLE_NAME", value: "fact" },
+    { type: "EXECUTABLE_NAME", value: "mul" },
+    { type: "CLOSE_CURLY_BRACKET" },
+    { type: "EXECUTABLE_NAME", value: "if" },
+    { type: "CLOSE_CURLY_BRACKET" },
+    { type: "EXECUTABLE_NAME", value: "def" },
+  ]);
+
+  list.length = 0;
+
+  for await (const token of tokenizer(helloWorld)) {
     list.push(token);
   }
 
